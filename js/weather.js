149a15de66ca0fd8886732225ae516a1dfc820be
wjdals0509 <wjdals050932@gmail.com>
2023-02-03 01:27:08 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,23 +1,22 @@
-const API_KEY = config.api_key;
-
 function onGeoOK(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    //javascript가 url을 호출하고 response를 받음 + response의 json을 얻음
-    //내용을 추출했으면 data를 얻음
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const weather = document.querySelector("#weather span:first-child");
-            const city = document.querySelector("#weather span:last-child");
-            city.innerText =  data.name;
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}ºC`;
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const API_KEY = config.apikey;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  //javascript가 url을 호출하고 response를 받음 + response의 json을 얻음
+  //내용을 추출했으면 data를 얻음
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}ºC`;
     });
 }
 
 function onGeoError() {
-    alert("Can't find you. No weather for you.");
+  alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
